c9833189d6d96eb3b1a00df514be88d1708674de
Shikhar Mohan <42655650+shikharmn@users.noreply.github.com>
2021-06-08 12:21:31 +0530

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -7,7 +7,7 @@ def cuda(x):
 
 
 class LossMulti:
-    def __init__(self, jaccard_weight=0, class_weights=None, num_classes=1):
+    def __init__(self, jaccard_weight=0, dice_weight=0, class_weights=None, num_classes=1):
         if class_weights is not None:
             nll_weight = cuda(
                 torch.from_numpy(class_weights.astype(np.float32)))
@@ -15,10 +15,11 @@ class LossMulti:
             nll_weight = None
         self.nll_loss = nn.NLLLoss2d(weight=nll_weight)
         self.jaccard_weight = jaccard_weight
+        self.dice_weight = dice_weight
         self.num_classes = num_classes
 
     def __call__(self, outputs, targets):
-        loss = (1 - self.jaccard_weight) * self.nll_loss(outputs, targets)
+        loss = (1 - self.jaccard_weight - self.dice_weight) * self.nll_loss(outputs, targets)
 
         if self.jaccard_weight:
             eps = 1e-15
@@ -28,5 +29,8 @@ class LossMulti:
                 intersection = (jaccard_output * jaccard_target).sum()
 
                 union = jaccard_output.sum() + jaccard_target.sum()
-                loss -= torch.log((intersection + eps) / (union - intersection + eps)) * self.jaccard_weight
+                J = (intersection + eps) / (union - intersection + eps)
+                D = 2 * J / (1 + J)
+                loss -= torch.log(J) * self.jaccard_weight
+                loss -= torch.log(D) * self.dice_weight
         return loss
